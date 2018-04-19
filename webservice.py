e90a29e53e09318e38987a82f283468f2dbdde64
PtitePierre <ptite_pierre71@hotmail.fr>
2018-04-19 13:32:33 +0100

    Update webservice.py
    remove MongoDB parts for MySQL

--- webservice.py
+++ webservice.py
@@ -1,14 +1,32 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
-from pymongo import MongoClient
-from mongoengine import *
-
-app = Flask(__name__)
+import MySQLdb
 
 
-class Unit(Document):
-    name = StringField(required=True, max_length=200)
+def connect():
+    """connect user to DataBase on host"""
+    db = MySQLdb.connect("host","user","pswd","DataBase")
+    return db
+
+def insertUnit(u_name, u_code):
+    db = connect()
+    cursor = db.cursor()
+    sql = "INSERT INTO UNIT(NAME, \
+       CODE) \
+       VALUES ('%s', '%s')" % \
+       (u_name, u_code)
+    try:
+       # Execute the SQL command
+       cursor.execute(sql)
+       # Commit your changes in the database
+       db.commit()
+    except:
+       # Rollback in case there is any error
+       db.rollback()
+
+    # disconnect from server
+    db.close()
 
 
 @app.route("/")
@@ -19,6 +37,28 @@ def hello():
 # TO DO : GET Units
 @app.route('/units', methods=['GET'])
 def getAllUnits():
+    db = connect()
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM UNIT"
+    try:
+       # Execute the SQL command
+       cursor.execute(sql)
+       # Fetch all the rows in a list of lists.
+       results = cursor.fetchall()
+        units = []
+       for row in results:        
+            unit = {
+                'name': row[0],
+                'code': row[1]
+            }
+            units.append(unit)
+    except:
+       print "Error: unable to fecth data"
+
+    # disconnect from server
+    db.close()
+    """
     client = MongoClient()
     db = client.fitness
     res = db.units.find({})
@@ -26,31 +66,40 @@ def getAllUnits():
 
     for unit in res:
         units.append(str(unit))
-
+    """
     return jsonify({'units': units})
 
 
 # TO DO : GET SportType
 @app.route('/sports', methods=['GET'])
 def getAllSports():
+    db = connect()
+    cursor = db.cursor()
+    sql = "SELECT * FROM SPORT"
+    """
     client = MongoClient()
     db = client.fitness
     res = db.sports.find({})
+    """
     sports = []
 
     for sport in res:
         sports.append(str(sport))
-
     return jsonify({"sports": sports})
 
 
 # TO DO : GET Session with specific id
 @app.route('/sessions/<user>', methods=['GET'])
 def getAllSessionsOfUser(user):
+    db = connect()
+    cursor = db.cursor()
+    sql = "SELECT * FROM SESSION WHERE USERID = '%d'" %(userid)
+    """
     client = MongoClient()
     db = client.fitness
     user_id = db.users.find({'name': user})
     res = db.sessions.find({'user_id': user_id})
+    """
     sessions = []
 
     for session in res:
@@ -61,8 +110,6 @@ def getAllSessionsOfUser(user):
 
 @app.route('/units', methods=['POST'])
 def createUnit():
-    client = MongoClient()
-    db = client.fitness
     unit = {
         'id': request.json['id'],
         'name': request.json['name'].lower(),
@@ -74,8 +121,6 @@ def createUnit():
 
 @app.route('/sports', methods=['POST'])
 def createSport():
-    client = MongoClient()
-    db = client.fitness
     sport = {
         'id': request.json['id'],
         'name': request.json['name'].lower()
@@ -86,8 +131,6 @@ def createSport():
 
 @app.route('/sessions/<user>', methods=['POST'])
 def createSession(user):
-    client = MongoClient()
-    db = client.fitness
     sport_id = db.sports.find({'name': request.json['sport_name']}).sport_id
     unit_id = db.units.find({'code': request.json['unit_code']}).unit_id
     user_id = db.users.find({'name': user}).user_id
@@ -106,14 +149,15 @@ def createSession(user):
 
 @app.route('/users', methods=['POST'])
 def createUser():
-    client = MongoClient()
-    db = client.fitness
     user = {
             'name': request.json['name'],
             'mail': request.json['mail'],
             }
     db.users.insert(user)
     return user
+
+
+app = Flask(__name__)
 
 
 if __name__ == '__main__':
