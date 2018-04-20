368c09b24dfe1b0243ac0a8cefb5930b11d9d780
PtitePierre <ptite_pierre71@hotmail.fr>
2018-04-20 17:25:17 +0100

    still testing

--- webservice.py
+++ webservice.py
@@ -162,6 +162,7 @@ def createSport():
         sport_id = cursor.fetchone()
 
         for unit in units:
+            print(unit['unit_id'])
             sql = "INSERT INTO sport_unit(sport_id, unit_id) \
                VALUES ('%d', '%d')" % (sport_id, unit['unit_id'])
             try:
